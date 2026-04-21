a330879d022cac10cc3d5e66c40c365ade72359b
Harsh Kulkarni <harsh_kulkarni_aids@moderncoe.edu.in>
2026-04-21 19:47:40 +0530

    Add EvenThread and OddThread for concurrent number printing
    Implement EvenThread and OddThread classes to print even and odd numbers concurrently.

---
+++ ThreadExample.java
@@ -0,0 +1,28 @@
+class EvenThread extends Thread {
+    public void run() {
+        System.out.println("Even Numbers:");
+        for(int i = 2; i <= 10; i += 2) {
+            System.out.println(i);
+        }
+    }
+}
+
+class OddThread extends Thread {
+    public void run() {
+        System.out.println("Odd Numbers:");
+        for(int i = 1; i <= 10; i += 2) {
+            System.out.println(i);
+        }
+    }
+}
+
+public class ThreadExample {
+    public static void main(String[] args) {
+
+        EvenThread t1 = new EvenThread();
+        OddThread t2 = new OddThread();
+
+        t1.start();  // start even thread
+        t2.start();  // start odd thread
+    }
+}
